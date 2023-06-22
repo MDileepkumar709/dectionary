f2f473b4d5491ec4acdb808dd182faa8bd34ffb9
MDileepkumar709 <136694346+MDileepkumar709@users.noreply.github.com>
2023-06-22 22:11:21 +0530

    Add files via upload

---
+++ dectionary.py
@@ -0,0 +1,12 @@
+asciidict = dict()
+for i in range(97,123):
+ asciidict [chr(i)]=i 
+
+key=asciidict.keys()
+value=asciidict.values()
+print('ASCII Dict:', asciidict)
+n= str(input('enter the key—>'))
+
+print('key:', n)
+
+print('value:', asciidict[n])
